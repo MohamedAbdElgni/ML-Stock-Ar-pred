a0463d71b66da510c09e325586db1ad7dfaba492
Mohamed Abd-El-Gani <45873305+bismohamed@users.noreply.github.com>
2023-05-13 04:07:08 +0300

    Rename get_data_daily

--- get_data_daily.py
+++ get_data_daily.py
@@ -9,22 +9,23 @@ from arch import arch_model
 
 db_name = "stocks.sqlite"
 
-connection=sqlite3.connect(database=db_name,check_same_thread=False)
+connection = sqlite3.connect(database=db_name, check_same_thread=False)
 
-repo=SQLrepo(connection=connection)
+repo = SQLrepo(connection=connection)
 
 
-ls_tick={
-    "Apple":"AAPL",
-    "IBM":"IBM",
-    "Microsoft":"MSFT",
-    "Google":"GOOG",
-    "Tesla":"TSLA",
-    "Amazon":"AMZN",
-    "META":"META",
+ls_tick = {
+    "Apple": "AAPL",
+    "IBM": "IBM",
+    "Microsoft": "MSFT",
+    "Google": "GOOG",
+    "Tesla": "TSLA",
+    "Amazon": "AMZN",
+    "META": "META",
 }
 
-def process_stock(ticker,table_name):
+
+def process_stock(ticker, table_name):
     data = YahooFinance(ticker)
     print(f"Trying Downloading data for {table_name} from Yahoo Finance API")
     data.get_data()
@@ -33,23 +34,22 @@ def process_stock(ticker,table_name):
     else:
         print(f"Data for {table_name} downloaded !!!!")
         repo.insert_table(table_name=table_name, records=data.data)
-        
+
 
 if __name__ == '__main__':
     start_time = time.time()
-    
+
     processes = []
-    for  table_name,ticker in ls_tick.items():
-        p = multiprocessing.Process(target=process_stock, args=(ticker,table_name))
+    for table_name, ticker in ls_tick.items():
+        p = multiprocessing.Process(
+            target=process_stock, args=(ticker, table_name))
         processes.append(p)
         p.start()
-    
+
     for p in processes:
         p.join()
 
     end_time = time.time()  # Record the end time
-    execution_time = end_time - start_time  # Calculate the execution time in seconds
+    # Calculate the execution time in seconds
+    execution_time = end_time - start_time
     print(f"Total execution time: {execution_time} seconds")
-
-
-
